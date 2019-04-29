db80f68ec9bc75e917986794df0213bdc6d04646
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-04-29 15:13:07 +1200

    Added project

---
+++ OrokonuiMobile/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
