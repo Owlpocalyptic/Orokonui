994211022c9ccee0518c545429e92d8bc4ed9469
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-06-10 13:47:14 +1200

    Made caching work better

--- kakasite/kakas/static/js/serviceworker.js
+++ kakasite/kakas/static/js/serviceworker.js
@@ -4,7 +4,10 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/base_layout'
+        '/kaka-quest',
+        '/kaka-quest/about',
+        '/kaka-quest/contact',
+        '/kaka-quest/downloads'
       ]);
     })
   );
